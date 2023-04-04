20c5b9c312d7bb4f0dfd3cc1fd6e743c3327c735
Ana Pessan <107801579+anacpessan@users.noreply.github.com>
2023-04-04 20:52:43 -0300

    Delete .py

--- .py
+++
@@ -1,5 +0,0 @@
-#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
-
-valor = input('Qual o valor da sua hora de trabalho?')
-horas = input('Quantas horas você trabalha no mês?')
-salario = valor*horas
